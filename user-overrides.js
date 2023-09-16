e71e2821524113a118c12a7a1e886656538dbf85
Thitat Auareesuksakul <thitat@flux.ci>
2023-09-16 18:21:09 +0900

    Deny all permission requests from websites by default

--- user-overrides.js
+++ user-overrides.js
@@ -57,6 +57,18 @@ user_pref("webgl.disabled", false);
  * [NOTE] This does not clear any passwords already saved
  * [SETTING] Privacy & Security>Logins and Passwords>Ask to save logins and passwords for websites ***/
 user_pref("signon.rememberSignons", false);
+/* 7002: set default permissions
+ * Location, Camera, Microphone, Notifications [FF58+] Virtual Reality [FF73+]
+ * 0=always ask (default), 1=allow, 2=block
+ * [WHY] These are fingerprintable via Permissions API, except VR. Just add site
+ * exceptions as allow/block for frequently visited/annoying sites: i.e. not global
+ * [SETTING] to add site exceptions: Ctrl+I>Permissions>
+ * [SETTING] to manage site exceptions: Options>Privacy & Security>Permissions>Settings ***/
+user_pref("permissions.default.geo", 2);
+user_pref("permissions.default.camera", 2);
+user_pref("permissions.default.microphone", 2);
+user_pref("permissions.default.desktop-notification", 2);
+user_pref("permissions.default.xr", 2); // Virtual Reality
 
 // Custom rule: Don't fast fallback to IPv4
 user_pref("network.http.fast-fallback-to-IPv4", false);
